454c3ea103bc21a62102a2f58b8029a393f7ac1c
AHHHZ975 <ahz.975@yahoo.com>
2021-04-04 01:12:08 -0400

    Final application v1.0

--- music.py
+++ music.py
@@ -15,16 +15,24 @@ class Music(flask.views.MethodView):
     def post(self):
         searchText = flask.request.form['search']
         Search.getMusic(Search, searchText)
-        # Search.getMusicInformation(Search, searchText)
+        Search.getMusicInformation(Search, searchText)
+
         with open('./db/sounds.json') as f:
             musicInformation = json.load(f) 
+
+        with open('./db/lyrics.json') as f:
+            musicLyric = json.load(f)             
+        
+        # Music information
         artist = musicInformation[0]['artist']
         title = musicInformation[0]['title']
         time = musicInformation[0]['time']
         downloadLink = musicInformation[0]['download_link']
+        lyric = musicLyric[0]['lyric']
+
         # download the music
         r = requests.get(downloadLink, allow_redirects=True)
         open(f'./static/music/{title}.mp3', 'wb').write(r.content)
         songs = os.listdir('./static/music')
-        return flask.render_template("music.html", songs=songs, artist=artist, title=title, time=time, downloadLink=downloadLink)
+        return flask.render_template("music.html", songs=songs, lyric=lyric, artist=artist, title=title, time=time, downloadLink=downloadLink)
         # return flask.redirect(flask.url_for('music'))
